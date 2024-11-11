bc88b10a6b521ef8875c8da49e5844b23054f543
iamsantoshku <118111660+iamsantoshku@users.noreply.github.com>
2024-11-11 09:36:53 +0530

    Update verifyToken.js

--- backend/auth/verifyToken.js
+++ backend/auth/verifyToken.js
@@ -1,58 +1,58 @@
-// import jwt from "jsonwebtoken";
-// import User from "../models/userSchema.js";
+import jwt from "jsonwebtoken";
+import User from "../models/userSchema.js";
 
 
-// import dotenv from 'dotenv';
-// dotenv.config();
+import dotenv from 'dotenv';
+dotenv.config();
 
-// export const authenticate = async (req, res, next) => {
-//   const authToken = req.headers.authorization;
-//   console.log(authToken, "Received Auth Token");
+export const authenticate = async (req, res, next) => {
+  const authToken = req.headers.authorization;
+  console.log(authToken, "Received Auth Token");
 
-//   if (!authToken || !authToken.startsWith("Bearer ")) {
-//     console.log("Token not found or invalid format");
-//     return res.status(401).json({ success: false, message: "Token not found" });
-//   }
+  if (!authToken || !authToken.startsWith("Bearer ")) {
+    console.log("Token not found or invalid format");
+    return res.status(401).json({ success: false, message: "Token not found" });
+  }
 
-//   try {
-//     const token = authToken.split(" ")[1];
-//     const decoded = jwt.verify(token, process.env.jhh);
+  try {
+    const token = authToken.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.jhh);
    
 
-//     req.userId = decoded.userId;
-//     console.log("Decoded user ID:", req.userId);
+    req.userId = decoded.userId;
+    console.log("Decoded user ID:", req.userId);
 
-//     next();
-//   } catch (error) {
-//     console.log("Error during token verification:", error);
+    next();
+  } catch (error) {
+    console.log("Error during token verification:", error);
 
-//     if (error.name === "TokenExpiredError") {
-//       return res.status(401).json({ success: false, message: "Session Expired" });
-//     } else if (error.name === "JsonWebTokenError") {
-//       return res.status(401).json({ success: false, message: "Invalid Token" });
-//     }
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ success: false, message: "Session Expired" });
+    } else if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({ success: false, message: "Invalid Token" });
+    }
 
-//     return res.status(401).json({ success: false, message: "Unauthorized" });
-//   }
-// };
+    return res.status(401).json({ success: false, message: "Unauthorized" });
+  }
+};
 
 
-// export const restrict = (roles) => async (req, res, next) => {
-//   const userId = req.userId;
-//   const user = await User.findById(userId);
+export const restrict = (roles) => async (req, res, next) => {
+  const userId = req.userId;
+  const user = await User.findById(userId);
 
-//   if (!user) {
-//     return res.status(401).json({ success: false, message: "user not found with given id" });
-//   }
+  if (!user) {
+    return res.status(401).json({ success: false, message: "user not found with given id" });
+  }
 
-//   console.log(user, "user 1");
+  console.log(user, "user 1");
 
-//   if (!roles.includes(user.role)) {
-//     return res.status(401).json({ success: false, message: "role is not matching" });
-//   }
+  if (!roles.includes(user.role)) {
+    return res.status(401).json({ success: false, message: "role is not matching" });
+  }
 
-//   next();
-// };
+  next();
+};
 
 
 
